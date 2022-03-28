34dcc605df0d24e82af54e04668d71eb93895989
Daniel Walder <daniel.walder@gmail.com>
2022-03-28 18:35:57 +1000

    Username and email were too long

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,8 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
-            $table->string('email')->unique();
+            $table->string('name', 200)->unique();
+            $table->string('email', 200)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('legacy_password', 60);
