95dd51c78163cf8c77b7d6ce2663d8da2d1134dd
Daniel Walder <daniel.walder@gmail.com>
2022-12-09 19:02:57 +1000

    Add mobile navigation, package updates

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,2 @@
 require('./bootstrap');
+window.Offcanvas = require('bootstrap/js/dist/offcanvas')
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,4 +1,88 @@
-<header class="container site-header px-0 d-flex flex-column">
+
+<header class="container site-header-mobile px-0 d-lg-none">
+    <nav class="navbar navbar-dark bg-dark p-0">
+        <div class="container-fluid p-0 m-0">
+            <span class="navbar-brand p-0">
+                <img src="{{ asset('images/header_logo.png') }}" alt="The Snarkpit" />
+            </span>
+            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#mobile-menu">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="mobile-menu">
+                <div class="offcanvas-header">
+                    <h5 class="offcanvas-title">The Snarkpit</h5>
+                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas"></button>
+                </div>
+                <div class="offcanvas-body">
+                    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                        @auth
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ url('panel') }}">control panel</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ url('message') }}">
+                                    @if ($num_unread_messages > 0)
+                                        <strong class="text-danger">messages ({{$num_unread_messages}})</strong>
+                                    @else
+                                        messages
+                                    @endif
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">updates</a>
+                            </li>
+                            @can('admin')
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ url('admin') }}">admin</a>
+                                </li>
+                            @endcan
+                            <li class="nav-item">
+                                <form method="post" action="{{ route('logout') }}">
+                                    @csrf
+                                    <button class="nav-link">logout</button>
+                                </form>
+                            </li>
+                        @else
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">login</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('register') }}">register</a>
+                            </li>
+                        @endauth
+                        <li class="nav-item">
+                            <hr/>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ url('/') }}">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ url('map') }}">Maps</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ url('article') }}">Articles</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ url('download') }}">Downloads</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ url('forum') }}">Forums</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ url('link') }}">Links</a>
+                        </li>
+                    </ul>
+                    <form class="d-flex mt-3" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-success" type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </nav>
+</header>
+
+<header class="container site-header px-0 d-none d-lg-flex flex-column">
     <div class="d-flex flex-row">
         <a href="{{ url('/') }}" class="home-link">
             <img src="{{ asset('images/header_logo.png') }}" alt="The Snarkpit" />
