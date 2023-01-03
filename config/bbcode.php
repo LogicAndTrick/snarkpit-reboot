7aa60f4f506f3c1663d84814cb31a99877241d19
Daniel Walder <daniel.walder@gmail.com>
2023-01-03 15:30:20 +1000

    Minor fixes for the bbcode editor

--- config/bbcode.php
+++ config/bbcode.php
@@ -1,7 +1,14 @@
 <?php
 
 use LogicAndTrick\WikiCodeParser\ParserConfiguration;
+use LogicAndTrick\WikiCodeParser\Processors\SmiliesProcessor;
 
 $conf = ParserConfiguration::Snarkpit();
+
+foreach ($conf->processors as $p) {
+    if ($p instanceof SmiliesProcessor) {
+        $p->urlFormatString = '/images/smilies/{0}.gif';
+    }
+}
 
 return $conf;
